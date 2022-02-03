400d783b96920707e2093835ce87b1a679f07b48
Louis <dupouylouis50@gmail.com>
2022-02-03 21:02:55 +0100

    Update

--- bnav.php
+++ bnav.php
@@ -1,8 +1,8 @@
 <nav id="Bnav">
-    <li>
-        <a href="accueil.php" target="blank">Accueil</a>
-        <a href="menus.php" target="blank">Nos menus</a>
-        <a href="contact.php" target="blank">Contact</a>
-        <a href="index.php" id="Title">L'éternel</a>
-    </li>
+    <h1>L'éternel</h1>
+    <div class="onglets">
+        <a href="#">Accueil</a>
+        <a href="#menus">Nos menus</a>
+        <a href="#contact">Contact</a>
+    </div>
 </nav>
