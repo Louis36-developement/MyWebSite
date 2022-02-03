cf0f7fbb050c5037a79612acfa5a715a091c40ad
Louis <dupouylouis50@gmail.com>
2022-02-03 20:36:52 +0100

    first commit

---
+++ bnav.php
@@ -0,0 +1,8 @@
+<nav id="Bnav">
+    <li>
+        <a href="accueil.php" target="blank">Accueil</a>
+        <a href="menus.php" target="blank">Nos menus</a>
+        <a href="contact.php" target="blank">Contact</a>
+        <a href="index.php" id="Title">L'éternel</a>
+    </li>
+</nav>
